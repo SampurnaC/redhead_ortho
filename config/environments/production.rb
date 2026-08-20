19cf682c498e00977f0cf0896b1e85fc7d2f15f9
sam <sampurna.chapagainnn@gmail.com>
2026-08-20 15:42:41 +0100

    update producion.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,17 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "netsol-smtp-oxcs.hostingplatform.com",
+    port: 587,
+    domain: "redheadorthodontics.com",
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
 end
